0de341a034f69cca632e00010b91bc61fe8d2968
Anders Latif <andl@kea.dk>
2020-09-01 10:17:26 +0200

    Added more string exercises

--- 1._Intro/1._Exercises/1.Basics.js
+++ 1._Intro/1._Exercises/1.Basics.js
@@ -9,6 +9,21 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
+//console.log("My first name is %s and my last name is %s", firstName, lastName)
+
+const introduction = "My first name is " + firstName + " and my last name is " + lastName;
+
+// console.log(firstName + " " + lastName);
+// console.log(firstName, lastName);
+
+// console.log(`My first name is ${firstName} and my last name is ${lastName}`);
+
+const stringTypeOne = "This is 'weird'";
+const stringTypeTwo = 'This is "weird"';
+const stringTypeThree = `"This" is 'weird'"'""'"`;
+
+
+
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -19,6 +34,19 @@ const number = 1;
 // Add the year plus the number
 // The result should be 2020
 // You cannot touch line 1 or 2
+
+// console.log(parseInt(year) + number);
+
+const currentYear = Number(year) + number;
+
+// console.log(currentYear);
+
+const testParsing = "123abc";
+
+console.log(Number(testParsing));
+
+console.log("" + 123);
+
 
 
 // --------------------------------------
