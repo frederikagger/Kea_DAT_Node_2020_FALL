e14017028b21f15678267bde3b3c21b484d47584
Anders Latif <andl@kea.dk>
2020-09-01 09:36:55 +0200

    Added the first String exercises

---
+++ 1._Intro/variables.js
@@ -0,0 +1,17 @@
+
+// accessing let before initialization throws an error
+// console.log(name);
+let name = "Anders";
+
+{
+    {
+        let scopedVar = 123;
+    }
+    // let sticks to its scope or below
+    // console.log(scopedVar);
+}
+
+
+
+
+
