cbd11972d6069842e230bd97df39fa1412eb03ab
Anders Latif <andl@kea.dk>
2020-09-08 11:31:47 +0200

    Upgraded the get request

--- 2._Our_First_API/app.js
+++ 2._Our_First_API/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 // const app = require("express")();
 
-
+app.get("/", (req, res) => {
+    return res.send("<h1>hello</h1>");
+});
 
 app.listen(8080, (error) => {
     if (error) {
