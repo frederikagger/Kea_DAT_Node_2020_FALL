a6943953a63730499a6adcec1cdcc120490dcc23
Anders Latif <andl@kea.dk>
2020-09-15 11:29:04 +0200

    Created the api file

--- 2._Our_First_API/app.js
+++ 2._Our_First_API/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 // const app = require("express")();
 
+app.use(express.urlencoded());
 app.use(express.json());
 
 app.get("/", (req, res) => {
