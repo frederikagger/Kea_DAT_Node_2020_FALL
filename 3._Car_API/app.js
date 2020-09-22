d6a0a235158c93c0db1045fce731aee12d9008e2
Anders Latif <andl@kea.dk>
2020-09-22 10:38:01 +0200

    Added cross-env and now supporting setting the port dynamically

--- 3._Car_API/app.js
+++ 3._Car_API/app.js
@@ -14,6 +14,10 @@ app.get("/", (req, res) => {
     return res.send({ data: "Welcome to the car API version 0.0.1" });
 });
 
+app.get("/cars", (req, res) => {
+    return res.send({ data: cars });
+});
+
 app.get("/cars/:id", (req, res) => {
     const car = cars.find(car => car.id === Number(req.params.id));
     return res.send({ data: car });
@@ -23,17 +27,32 @@ app.post("/cars", (req, res) => {
     const newCar = req.body;
     newCar.id = nextCarId++;
     cars.push(newCar);
+    return res.send({ data: newCar });
+});
+
+app.patch("/cars/:id", (req, res) => {
+    cars = cars.map(car => {
+        if (car.id === Number(req.params.id)) {
+            return { ...car, ...req.body, id: car.id };
+        }
+        return car;
+    });
     return res.send({ data: cars });
 });
 
-app.get("/cars", (req, res) => {
+app.delete("/cars/:id", (req, res) => {
+    cars = cars.filter(car => car.id !== Number(req.params.id));
     return res.send({ data: cars });
 });
 
+// environment variables
+const port = process.env.PORT || 80;
+// ternary statement
+// const port = process.env.PORT ? process.env.PORT : 80;
 
-app.listen(8080, (error) => {
+app.listen(port, (error) => {
     if (error) {
         console.log("Error starting the server");
     }
-    console.log("Server started on port", 8080);
+    console.log("Server started on port", Number(port));
 });
